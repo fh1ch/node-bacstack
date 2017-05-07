320366af9db3edfeb8988d243c99c5990d830f44
Fabio Huser <fabio.huser@siemens.com>
2017-05-07 14:45:37 +0200

    test(read-property-multiple): add integration tests using real recorded data

--- test/integration/utils.js
+++ test/integration/utils.js
@@ -2,3 +2,27 @@ var bacnet = require('../../');
 
 module.exports.bacnetClient = bacnet;
 module.exports.globalBacnetClient = bacnet();
+
+module.exports.transportStub = function() {
+  var self = this;
+  self.handler = function() {};
+  self.setHandler = function(handler) {
+    self.handler = handler;
+  };
+  self.getBroadcastAddress = function() {
+    return '255.255.255.255';
+  };
+  self.getMaxPayload = function() {
+    return 1472;
+  };
+  self.send = function() {};
+  return self;
+};
+
+module.exports.propertyFormater = function(object) {
+  var converted = {};
+  object.forEach((property) => {
+    converted[property.propertyIdentifier] = property.value;
+  });
+  return converted;
+};
