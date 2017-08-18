12f27efde96165a0685793d45bed0030f3be8352
Fabio Huser <fabio@fh1.ch>
2017-08-18 12:56:53 +0200

    refactor(bacnet-client): implement options parameter to client functions
    BREAKING CHANGE: various function parameters changed. Adapt according latest documentation.

--- test/integration/read-property.spec.js
+++ test/integration/read-property.spec.js
@@ -4,7 +4,7 @@ var utils = require('./utils');
 describe('bacstack - readProperty integration', function() {
   it('should return a timeout error if no device is available', function(next) {
     var client = new utils.bacnetClient({adpuTimeout: 200});
-    client.readProperty('127.0.0.1', 8, 44301, 28, null, function(err, value) {
+    client.readProperty('127.0.0.1', 8, 44301, 28, function(err, value) {
       expect(err.message).to.eql('ERR_TIMEOUT');
       expect(value).to.eql(undefined);
       client.close();
--- test/integration/reinitialize-sevice.spec.js
+++ test/integration/reinitialize-sevice.spec.js
@@ -4,7 +4,7 @@ var utils = require('./utils');
 describe('bacstack - reinitializeDevice integration', function() {
   it('should return a timeout error if no device is available', function(next) {
     var client = new utils.bacnetClient({adpuTimeout: 200});
-    client.reinitializeDevice('127.0.0.1', 1, 'Test1234',  function(err, value) {
+    client.reinitializeDevice('127.0.0.1', 1, {password: 'Test1234'},  function(err, value) {
       expect(err.message).to.eql('ERR_TIMEOUT');
       expect(value).to.eql(undefined);
       client.close();
