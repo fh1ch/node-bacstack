635e41954ffd95a4b0cff252581f380981d4d278
Fabio Huser <fabio@fh1.ch>
2017-08-21 08:03:24 +0200

    feat(bacnet-service): implement decoding functionality for COV and CreateObject

--- test/unit/bacnet-services.spec.js
+++ test/unit/bacnet-services.spec.js
@@ -1594,4 +1594,176 @@ describe('bacstack - Services layer', function() {
       });
     });
   });
+
+  describe('CreateObject', function() {
+    it('should successfully encode and decode', function() {
+      var buffer = utils.getBuffer();
+      var date = new Date(1, 1, 1);
+      var time = new Date(1, 1, 1);
+      time.setMilliseconds(990);
+      baServices.encodeCreateObject(buffer, {type: 1, instance: 10}, [
+        {property: {propertyIdentifier: 81, propertyArrayIndex: 0xFFFFFFFF}, value: [
+          {type: 1, value: true},
+          {type: 1, value: false},
+          {type: 2, value: 1},
+          {type: 2, value: 1000},
+          {type: 2, value: 1000000},
+          {type: 2, value: 1000000000},
+          {type: 3, value: -1},
+          {type: 3, value: -1000},
+          {type: 3, value: -1000000},
+          {type: 3, value: -1000000000},
+          {type: 4, value: 0.1},
+          {type: 5, value: 100.121212},
+          {type: 6, value: [1, 2, 100, 200]},
+          {type: 7, value: 'Test1234$'},
+          {type: 8, value: {bitsUsed: 0, value: []}},
+          {type: 8, value: {bitsUsed: 24, value: [0xAA, 0xAA, 0xAA]}},
+          {type: 9, value: 4},
+          {type: 10, value: date},
+          {type: 11, value: time}
+        ], priority: 0},
+        {property: {propertyIdentifier: 82, propertyArrayIndex: 0}, value: [
+          {type: 12, value: {type: 3, instance: 0}}
+        ], priority: 0}
+      ]);
+      var result = baServices.decodeCreateObject(buffer.buffer, 0, buffer.offset);
+      delete result.len;
+      result.values[0].value[10].value = Math.floor(result.values[0].value[10].value * 1000) / 1000;
+      expect(result).to.deep.equal({
+        objectId: {
+          type: 1,
+          instance: 10
+        },
+        values: [
+          {
+            property: {
+              arrayIndex: 0xFFFFFFFF,
+              propertyId: 81
+            },
+            value: [
+              {type: 1, value: true, len: 1},
+              {type: 1, value: false, len: 1},
+              {type: 2, value: 1, len: 2},
+              {type: 2, value: 1000, len: 3},
+              {type: 2, value: 1000000, len: 4},
+              {type: 2, value: 1000000000, len: 5},
+              {type: 3, value: -1, len: 2},
+              {type: 3, value: -1000, len: 3},
+              {type: 3, value: -1000000, len: 4},
+              {type: 3, value: -1000000000, len: 5},
+              {type: 4, value: 0.1, len: 5},
+              {type: 5, value: 100.121212, len: 10},
+              {type: 6, value: [1, 2, 100, 200], len: 5},
+              {type: 7, value: 'Test1234$', encoding: 0, len: 12},
+              {type: 8, value: {bitsUsed: 0, value: []}, len: 2},
+              {type: 8, value: {bitsUsed: 24, value: [0xAA, 0xAA, 0xAA]}, len: 5},
+              {type: 9, value: 4, len: 2},
+              {type: 10, value: date, len: 5},
+              {type: 11, value: time, len: 5}
+            ]
+          },
+          {
+            property: {
+              arrayIndex: 0xFFFFFFFF,
+              propertyId: 82
+            },
+            value: [
+              {type: 12, value: {type: 3, instance: 0}, len: 5}
+            ]
+          }
+        ]
+      });
+    });
+  });
+
+  describe('COVNotify', function() {
+    it('should successfully encode and decode', function() {
+      var buffer = utils.getBuffer();
+      var date = new Date(1, 1, 1);
+      var time = new Date(1, 1, 1);
+      time.setMilliseconds(990);
+      baServices.encodeCOVNotify(buffer, 7, 443, {type: 2, instance: 12}, 120, [
+        {property: {propertyIdentifier: 81, propertyArrayIndex: 0xFFFFFFFF}, value: [
+          {type: 1, value: true},
+          {type: 1, value: false},
+          {type: 2, value: 1},
+          {type: 2, value: 1000},
+          {type: 2, value: 1000000},
+          {type: 2, value: 1000000000},
+          {type: 3, value: -1},
+          {type: 3, value: -1000},
+          {type: 3, value: -1000000},
+          {type: 3, value: -1000000000},
+          {type: 4, value: 0.1},
+          {type: 5, value: 100.121212},
+          {type: 6, value: [1, 2, 100, 200]},
+          {type: 7, value: 'Test1234$'},
+          {type: 8, value: {bitsUsed: 0, value: []}},
+          {type: 8, value: {bitsUsed: 24, value: [0xAA, 0xAA, 0xAA]}},
+          {type: 9, value: 4},
+          {type: 10, value: date},
+          {type: 11, value: time}
+        ], priority: 0},
+        {property: {propertyIdentifier: 82, propertyArrayIndex: 0}, value: [
+          {type: 12, value: {type: 3, instance: 0}}
+        ], priority: 8}
+      ]);
+      var result = baServices.decodeCOVNotify(buffer.buffer, 0, buffer.offset);
+      delete result.len;
+      result.values[0].value[10].value = Math.floor(result.values[0].value[10].value * 1000) / 1000;
+      expect(result).to.deep.equal({
+        initiatingDeviceIdentifier: {
+          type: 8,
+          instance: 443
+        },
+        monitoredObjectIdentifier: {
+          type: 2,
+          instance: 12
+        },
+        subscriberProcessIdentifier: 7,
+        timeRemaining: 120,
+        values: [
+          {
+            priority: 0,
+            property: {
+              arrayIndex: 0xFFFFFFFF,
+              propertyId: 81
+            },
+            value: [
+              {type: 1, value: true, len: 1},
+              {type: 1, value: false, len: 1},
+              {type: 2, value: 1, len: 2},
+              {type: 2, value: 1000, len: 3},
+              {type: 2, value: 1000000, len: 4},
+              {type: 2, value: 1000000000, len: 5},
+              {type: 3, value: -1, len: 2},
+              {type: 3, value: -1000, len: 3},
+              {type: 3, value: -1000000, len: 4},
+              {type: 3, value: -1000000000, len: 5},
+              {type: 4, value: 0.1, len: 5},
+              {type: 5, value: 100.121212, len: 10},
+              {type: 6, value: [1, 2, 100, 200], len: 5},
+              {type: 7, value: 'Test1234$', encoding: 0, len: 12},
+              {type: 8, value: {bitsUsed: 0, value: []}, len: 2},
+              {type: 8, value: {bitsUsed: 24, value: [0xAA, 0xAA, 0xAA]}, len: 5},
+              {type: 9, value: 4, len: 2},
+              {type: 10, value: date, len: 5},
+              {type: 11, value: time, len: 5}
+            ]
+          },
+          {
+            priority: 0,
+            property: {
+              arrayIndex: 0xFFFFFFFF,
+              propertyId: 82
+            },
+            value: [
+              {type: 12, value: {type: 3, instance: 0}, len: 5}
+            ]
+          }
+        ]
+      });
+    });
+  });
 });
