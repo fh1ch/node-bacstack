6dc940d25da2e4879edc1727f8631d8e5d0c10ad
Fabio Huser <fabio@fh1.ch>
2021-07-02 13:17:57 +0200

    refactor(live-safety-operation): rework to TypeScript

--- lib/services/life-safety-operation.ts
+++ lib/services/life-safety-operation.ts
@@ -1,19 +1,20 @@
 'use strict';
 
-const baAsn1 = require('../asn1');
+import * as baAsn1 from '../asn1';
+import { EncodeBuffer, BACNetObjectID } from '../types';
 
-module.exports.encode = (buffer, processId, requestingSource, operation, targetObjectId) => {
+export const encode = (buffer: EncodeBuffer, processId: number, requestingSource: string, operation: number, targetObjectId: BACNetObjectID) => {
   baAsn1.encodeContextUnsigned(buffer, 0, processId);
   baAsn1.encodeContextCharacterString(buffer, 1, requestingSource);
   baAsn1.encodeContextEnumerated(buffer, 2, operation);
   baAsn1.encodeContextObjectId(buffer, 3, targetObjectId.type, targetObjectId.instance);
 };
 
-module.exports.decode = (buffer, offset, apduLen) => {
+export const decode = (buffer: Buffer, offset: number, apduLen: number) => {
   let len = 0;
-  let result;
-  let decodedValue;
-  const value = {};
+  let result: any;
+  let decodedValue: any;
+  const value: any = {};
   result = baAsn1.decodeTagNumberAndValue(buffer, offset + len);
   len += result.len;
   decodedValue = baAsn1.decodeUnsigned(buffer, offset + len, result.value);
