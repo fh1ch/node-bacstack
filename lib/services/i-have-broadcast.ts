851c26d1e2e73fc380199f7248fbf0b22becc6bd
Fabio Huser <fabio@fh1.ch>
2021-07-02 13:17:57 +0200

    refactor(i-have-broadcast): rework to TypeScript

--- lib/services/i-have-broadcast.ts
+++ lib/services/i-have-broadcast.ts
@@ -1,18 +1,19 @@
 'use strict';
 
-const baAsn1 = require('../asn1');
+import * as baAsn1 from '../asn1';
+import { EncodeBuffer, BACNetObjectID } from '../types';
 
-module.exports.encode = (buffer, deviceId, objectId, objectName) => {
+export const encode = (buffer: EncodeBuffer, deviceId: BACNetObjectID, objectId: BACNetObjectID, objectName: string) => {
   baAsn1.encodeApplicationObjectId(buffer, deviceId.type, deviceId.instance);
   baAsn1.encodeApplicationObjectId(buffer, objectId.type, objectId.instance);
   baAsn1.encodeApplicationCharacterString(buffer, objectName);
 };
 
-module.exports.decode = (buffer, offset, apduLen) => {
+export const decode = (buffer: Buffer, offset: number, apduLen: number) => {
   let len = 0;
-  let result;
-  let decodedValue;
-  const value = {};
+  let result: any;
+  let decodedValue: any;
+  const value: any = {};
   result = baAsn1.decodeTagNumberAndValue(buffer, offset + len);
   len += result.len;
   decodedValue = baAsn1.decodeObjectId(buffer, offset + len);
