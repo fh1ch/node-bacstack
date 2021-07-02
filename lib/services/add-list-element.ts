6765d13d68c0b71cd12f29318d14ab172da7dcf1
Fabio Huser <fabio@fh1.ch>
2021-07-02 13:17:47 +0200

    refactor(add-list-element): rework to TypeScript

--- lib/services/add-list-element.ts
+++ lib/services/add-list-element.ts
@@ -1,26 +1,25 @@
 'use strict';
 
-const baAsn1 = require('../asn1');
-const baEnum = require('../enum');
+import * as baAsn1 from '../asn1';
+import * as baEnum from '../enum';
+import { EncodeBuffer, BACNetObjectID } from '../types';
 
-module.exports.encode = (buffer, objectId, propertyId, arrayIndex, values) => {
+export const encode = (buffer: EncodeBuffer, objectId: BACNetObjectID, propertyId: number, arrayIndex: number, values: any) => {
   baAsn1.encodeContextObjectId(buffer, 0, objectId.type, objectId.instance);
   baAsn1.encodeContextEnumerated(buffer, 1, propertyId);
   if (arrayIndex !== baEnum.ASN1_ARRAY_ALL) {
     baAsn1.encodeContextUnsigned(buffer, 2, arrayIndex);
   }
   baAsn1.encodeOpeningTag(buffer, 3);
-  values.forEach((value) => {
-    baAsn1.bacappEncodeApplicationData(buffer, value);
-  });
+  values.forEach((value: any) => baAsn1.bacappEncodeApplicationData(buffer, value));
   baAsn1.encodeClosingTag(buffer, 3);
 };
 
-module.exports.decode = (buffer, offset, apduLen) => {
+export const decode = (buffer: Buffer, offset: number, apduLen: number) => {
   let len = 0;
-  let result;
-  let decodedValue;
-  const value = {};
+  let result: any;
+  let decodedValue: any;
+  const value: any = {};
   result = baAsn1.decodeTagNumberAndValue(buffer, offset + len);
   len += result.len;
   decodedValue = baAsn1.decodeObjectId(buffer, offset + len);
@@ -30,15 +29,13 @@ module.exports.decode = (buffer, offset, apduLen) => {
   len += result.len;
   decodedValue = baAsn1.decodeEnumerated(buffer, offset + len, result.value);
   len += decodedValue.len;
-  value.property = {id: decodedValue.value};
+  value.property = {id: decodedValue.value, index: baEnum.ASN1_ARRAY_ALL};
   if (len < apduLen && baAsn1.decodeIsContextTag(buffer, offset + len, 2)) {
     result = baAsn1.decodeTagNumberAndValue(buffer, offset + len);
     len += result.len;
     decodedValue = baAsn1.decodeUnsigned(buffer, offset + len, result.value);
     len += decodedValue.len;
     value.property.index = decodedValue.value;
-  } else {
-    value.property.index = baEnum.ASN1_ARRAY_ALL;
   }
   const values = [];
   if (!baAsn1.decodeIsOpeningTagNumber(buffer, offset + len, 3)) return;
